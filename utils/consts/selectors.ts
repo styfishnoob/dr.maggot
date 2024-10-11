44901e8739b3bc5ac6db65b76c4b6b18ddbc2a3e
styfishnoob <main.sakana@gmail.com>
2024-10-11 13:07:20 +0900

    kickに対応

--- utils/consts/selectors.ts
+++ utils/consts/selectors.ts
@@ -1,9 +1,10 @@
 const NULL = "[drmaggot__null]";
 
-export const Selectors: PlatformSelectors = {
+export const Selectors: WebSelectors = {
     videoPlayer: {
         youtube: `#movie_player`,
         twitch: `[data-a-target="video-player"]`,
+        kick: `#injected-embedded-channel-player-video`,
         openrec: `.Component__VideoWrapper-sc-1x94c9l-6`,
         twicas: `.tw-player`,
     },
@@ -11,51 +12,51 @@ export const Selectors: PlatformSelectors = {
         container: {
             youtube: `#items`,
             twitch: `[data-test-selector="chat-scrollable-area__message-container"]`,
+            kick: `#chatroom-messages > div.no-scrollbar`,
             openrec: `.chat-list-content`,
             twicas: `.tw-comment-list-view__scroller > div`,
         },
         cell: {
             youtube: `yt-live-chat-text-message-renderer`,
             twitch: `[data-a-target="chat-line-message"], .video-chat__message-list-wrapper li`,
+            kick: `#chatroom-messages > div.no-scrollbar > div[data-index]`,
             openrec: `.ChatList__CellContainer-sc-69ljpb-1`,
             twicas: `.tw-comment-item`,
         },
         quickBlockAnchor: {
             youtube: `#menu`,
             twitch: `.chat-line__icons, .video-chat__message-menu`,
+            kick: `.group`,
             openrec: `.ChatCell__Cell-sc-1ey02g1-4`,
             twicas: `.tw-comment-item-dropdown`,
         },
         contents: {
             youtube: `#message`,
             twitch: `[data-a-target="chat-line-message-body"], .video-chat__message > span[class=""]`,
+            kick: `.font-normal`,
             openrec: `.chat-content`,
             twicas: `.tw-comment-item-comment`,
         },
         userName: {
             youtube: `#author-name`,
             twitch: `[data-a-user]`,
+            kick: `button[class="inline font-bold"]`,
             openrec: `.UserName__Name-sc-1i0rd20-1`,
             twicas: `.tw-comment-item-name`,
         },
         messages: {
             youtube: `#message`,
             twitch: `.text-fragment`,
+            kick: `.font-normal`,
             openrec: `.chat-content`,
             twicas: `.tw-comment-item-comment > span`,
         },
 
         emotes: {
             youtube: `.emoji`,
-            twitch: `.chat-line__message--emote`,
+            twitch: `.chat-line__message--emote-button`,
+            kick: `[data-emote-id][data-emote-name]`,
             openrec: `.ChatCell__StampContainer-sc-1ey02g1-7`,
-            twicas: `${NULL}`,
-        },
-
-        twitchBonus: {
-            youtube: `${NULL}`,
-            twitch: `[data-test-selector="community-points-summary"]`,
-            openrec: `${NULL}`,
             twicas: `${NULL}`,
         },
     },
--- utils/consts/settings.ts
+++ utils/consts/settings.ts
@@ -1,70 +1,78 @@
-import { KVManager } from '@/src/lib/kv-manager';
-import { MapManager } from '@/src/lib/map-manager';
+import { KVManager } from "@/src/lib/kv-manager";
+import { MapManager } from "@/src/lib/map-manager";
 
 export const KVManagerList = {
-    display: new KVManager<Display>('Display'),
-    filter: new KVManager<Filter>('Filter'),
-    danmaku: new KVManager<Danmaku>('Danmaku'),
-    other: new KVManager<Other>('Other'),
+    display: new KVManager<Display>("Display"),
+    filter: new KVManager<Filter>("Filter"),
+    danmaku: new KVManager<Danmaku>("Danmaku"),
+    other: new KVManager<Other>("Other"),
 };
 
 export const MapManagerList = {
-    word: new MapManager<BlockMaps, BlockItem>('BlockedWords'),
-    user: new MapManager<BlockMaps, BlockItem>('BlockedUsers'),
-    emote: new MapManager<BlockMaps, BlockItem>('BlockedEmotes'),
+    word: new MapManager<AllPlatformBlocklistRecord, BlocklistItem>("BlockedWords"),
+    user: new MapManager<AllPlatformBlocklistRecord, BlocklistItem>("BlockedUsers"),
+    emote: new MapManager<AllPlatformBlocklistRecord, BlocklistItem>("BlockedEmotes"),
 };
 
+const ALL_FALSE = { youtube: false, twitch: false, kick: false, openrec: false, twicas: false };
+const ALL_TRUE = { youtube: true, twitch: true, kick: true, openrec: true, twicas: true };
+
 const DefaultDisplay: Display = {
-    hideName: { youtube: false, twitch: false, openrec: false, twicas: false },
-    unifyName: { youtube: false, twitch: false, openrec: false, twicas: false },
+    hideName: ALL_FALSE,
+    unifyName: ALL_FALSE,
     unifyNameValue: `'@'`,
-    stripe: { youtube: false, twitch: false, openrec: false, twicas: false },
-    stripeColor: '#2563eb',
-    break: { youtube: false, twitch: false, openrec: false, twicas: false },
-    font: '',
+    stripe: ALL_FALSE,
+    stripeColor: "#2563eb",
+    break: ALL_FALSE,
+    font: "",
     fontSize: 13,
 };
 
 const DefaultFilter: Filter = {
-    filter: { youtube: true, twitch: true, openrec: true, twicas: true },
-    subOnly: { youtube: false, twitch: false, openrec: false, twicas: false },
+    filter: ALL_TRUE,
+    subOnly: ALL_FALSE,
     charLimit: { less: 0, more: 0 },
     emoteLimit: { less: 0, more: 0 },
-    range: { youtube: false, twitch: false, openrec: false, twicas: false },
+    range: ALL_FALSE,
 };
 
 const DefaultDanmaku: Danmaku = {
-    danmaku: { youtube: false, twitch: false, openrec: false, twicas: false },
-    font: '',
+    danmaku: ALL_FALSE,
+    font: "",
     fontSize: 28,
     opacity: 50,
     speed: 10,
 };
 
 const DefaultOther: Other = {
-    quickBlock: { youtube: false, twitch: false, openrec: false, twicas: false },
-    autoBonus: { youtube: false, twitch: false, openrec: false, twicas: false },
-    countdown: { youtube: false, twitch: false, openrec: false, twicas: false },
+    quickBlock: ALL_FALSE,
+    autoBonus: ALL_FALSE,
+    countdown: ALL_FALSE,
 };
 
-const DefaultBlockedWords: BlockMaps = {
+const DefaultBlockedWords: AllPlatformBlocklistRecord = {
     all: [],
     youtube: [],
     twitch: [],
+    kick: [],
     openrec: [],
     twicas: [],
 };
-const DefaultBlockedUsers: BlockMaps = {
+
+const DefaultBlockedUsers: AllPlatformBlocklistRecord = {
     all: [],
     youtube: [],
     twitch: [],
+    kick: [],
     openrec: [],
     twicas: [],
 };
-const DefaultBlockedEmotes: BlockMaps = {
+
+const DefaultBlockedEmotes: AllPlatformBlocklistRecord = {
     all: [],
     youtube: [],
     twitch: [],
+    kick: [],
     openrec: [],
     twicas: [],
 };
