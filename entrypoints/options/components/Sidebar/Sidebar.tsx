fe4e772642bc6f705f1393c3c7a9958c721e7185
styfishnoob <main.sakana@gmail.com>
2024-11-01 09:28:23 +0900

    githubのリンクを追加

--- entrypoints/options/components/Sidebar/Sidebar.tsx
+++ entrypoints/options/components/Sidebar/Sidebar.tsx
@@ -1,30 +1,32 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 
-import bell from '@/assets/icons/bell.svg';
-import book from '@/assets/icons/book.svg';
-import gem from '@/assets/icons/gem.svg';
-import megaphone from '@/assets/icons/megaphone.svg';
-import monitor from '@/assets/icons/monitor.svg';
-import other from '@/assets/icons/other.svg';
-import robot from '@/assets/icons/robot.svg';
-import users from '@/assets/icons/users.svg';
+import bell from "@/assets/icons/bell.svg";
+import book from "@/assets/icons/book.svg";
+import gem from "@/assets/icons/gem.svg";
+import megaphone from "@/assets/icons/megaphone.svg";
+import monitor from "@/assets/icons/monitor.svg";
+import other from "@/assets/icons/other.svg";
+import robot from "@/assets/icons/robot.svg";
+import users from "@/assets/icons/users.svg";
 
-import odaibako from '@/assets/link_icons/odaibako.png';
-import webstore from '@/assets/link_icons/svg-webstore.svg';
+import odaibako from "@/assets/link_icons/odaibako.png";
+import webstore from "@/assets/link_icons/webstore.svg";
+import github from "@/assets/link_icons/github.svg";
 
 const gradationList = {
-    red: 'bg-gradient-to-b from-red-400 via-red-600 to-red-600',
-    green: 'bg-gradient-to-b from-green-400 via-green-500 to-green-500',
-    blue: 'bg-gradient-to-b from-blue-400 via-blue-500 to-blue-500',
-    amber: 'bg-gradient-to-b from-amber-400 to-amber-500',
-    gray: 'bg-gradient-to-b from-stone-400 via-stone-600 to-stone-600',
-    slate: 'bg-gradient-to-r from-slate-400 to-slate-500',
+    black: "bg-gradient-to-t from-stone-900 to-neutral-700",
+    red: "bg-gradient-to-b from-red-400 via-red-600 to-red-600",
+    green: "bg-gradient-to-b from-green-400 via-green-500 to-green-500",
+    blue: "bg-gradient-to-b from-blue-400 via-blue-500 to-blue-500",
+    amber: "bg-gradient-to-b from-amber-400 to-amber-500",
+    gray: "bg-gradient-to-b from-stone-400 via-stone-600 to-stone-600",
+    slate: "bg-gradient-to-r from-slate-400 to-slate-500",
 };
 
 type SidebarColumnProps = {
     to: string;
     title: string;
-    src: string;
+    imgSrc: string;
     gradationKey: keyof typeof gradationList;
 };
 
@@ -33,6 +35,7 @@ type ExternalLinkProps = {
     target?: string;
     linkSrc: string;
     imgSrc: string;
+    gradationKey: keyof typeof gradationList;
 };
 
 const SidebarColumn = (props: SidebarColumnProps) => {
@@ -40,11 +43,15 @@ const SidebarColumn = (props: SidebarColumnProps) => {
         <li>
             <NavLink
                 to={props.to}
-                className={({ isActive }) => (isActive ? 'bg-blue-700 text-white' : 'bg-inherit text-black dark:text-white')}
+                className={({ isActive }) =>
+                    isActive ? "bg-blue-700 text-white" : "bg-inherit text-black dark:text-white"
+                }
             >
                 <div className="text-xm flex items-center gap-x-1.5 rounded-md bg-inherit px-1.5 py-1">
-                    <div className={`rounded-md p-1 shadow-sm ${gradationList[props.gradationKey]}`}>
-                        <img width={16} height={16} src={props.src} />
+                    <div
+                        className={`rounded-md p-1 shadow-sm ${gradationList[props.gradationKey]}`}
+                    >
+                        <img width={16} height={16} src={props.imgSrc} />
                     </div>
                     <div>
                         <span>{props.title}</span>
@@ -58,9 +65,15 @@ const SidebarColumn = (props: SidebarColumnProps) => {
 const ExternalLink = (props: ExternalLinkProps) => {
     return (
         <li>
-            <a className="text-black dark:text-white" target={props.target ? props.target : '_self'} href={props.linkSrc}>
+            <a
+                className="text-black dark:text-white"
+                target={props.target ? props.target : "_self"}
+                href={props.linkSrc}
+            >
                 <div className="text-xm flex items-center gap-x-1.5 rounded-md bg-inherit px-1.5 py-1">
-                    <div className={`rounded-md p-1 shadow-sm ${gradationList.slate}`}>
+                    <div
+                        className={`rounded-md p-1 shadow-sm ${gradationList[props.gradationKey]}`}
+                    >
                         <img width={16} height={16} src={props.imgSrc} />
                     </div>
                     <div>
@@ -80,36 +93,43 @@ const Sidebar = () => {
                 className="w-56 min-w-56 max-w-56 overflow-y-auto border-e border-gray-200 bg-stone-100 pb-10 pt-7 dark:border-neutral-700 dark:bg-neutral-900"
             >
                 <div className="px-6">
-                    <a className="flex-none text-xl font-semibold text-black dark:text-white" href="#" aria-label="Dr.Maggot">
+                    <a
+                        className="flex-none text-xl font-semibold text-black dark:text-white"
+                        href="#"
+                        aria-label="Dr.Maggot"
+                    >
                         <span>Dr.Maggot</span>
                     </a>
                 </div>
-                <nav className="hs-accordion-group flex w-full flex-col flex-wrap p-6" data-hs-accordion-always-open>
+                <nav
+                    className="hs-accordion-group flex w-full flex-col flex-wrap p-6"
+                    data-hs-accordion-always-open
+                >
                     <ul className="space-y-3">
                         <li>
                             <ul>
                                 <SidebarColumn
                                     to="/"
-                                    title={browser.i18n.getMessage('menu_display')}
-                                    src={monitor}
+                                    title={browser.i18n.getMessage("menu_display")}
+                                    imgSrc={monitor}
                                     gradationKey="green"
                                 />
                                 <SidebarColumn
                                     to="/filter"
-                                    title={browser.i18n.getMessage('menu_filter')}
-                                    src={robot}
+                                    title={browser.i18n.getMessage("menu_filter")}
+                                    imgSrc={robot}
                                     gradationKey="blue"
                                 />
                                 <SidebarColumn
                                     to="/danmaku"
-                                    title={browser.i18n.getMessage('menu_danmaku')}
-                                    src={megaphone}
+                                    title={browser.i18n.getMessage("menu_danmaku")}
+                                    imgSrc={megaphone}
                                     gradationKey="amber"
                                 />
                                 <SidebarColumn
                                     to="/other"
-                                    title={browser.i18n.getMessage('menu_other')}
-                                    src={other}
+                                    title={browser.i18n.getMessage("menu_other")}
+                                    imgSrc={other}
                                     gradationKey="gray"
                                 />
                             </ul>
@@ -118,20 +138,20 @@ const Sidebar = () => {
                             <ul>
                                 <SidebarColumn
                                     to="/blocklist-word"
-                                    title={browser.i18n.getMessage('menu_blockedWords')}
-                                    src={book}
+                                    title={browser.i18n.getMessage("menu_blockedWords")}
+                                    imgSrc={book}
                                     gradationKey="red"
                                 />
                                 <SidebarColumn
                                     to="/blocklist-user"
-                                    title={browser.i18n.getMessage('menu_blockedUsers')}
-                                    src={users}
+                                    title={browser.i18n.getMessage("menu_blockedUsers")}
+                                    imgSrc={users}
                                     gradationKey="red"
                                 />
                                 <SidebarColumn
                                     to="/blocklist-emote"
-                                    title={browser.i18n.getMessage('menu_blockedEmotes')}
-                                    src={gem}
+                                    title={browser.i18n.getMessage("menu_blockedEmotes")}
+                                    imgSrc={gem}
                                     gradationKey="red"
                                 />
                             </ul>
@@ -140,21 +160,33 @@ const Sidebar = () => {
                             <ul>
                                 <SidebarColumn
                                     to="/notice"
-                                    title={browser.i18n.getMessage('menu_notice')}
-                                    src={bell}
+                                    title={browser.i18n.getMessage("menu_notice")}
+                                    imgSrc={bell}
                                     gradationKey="slate"
                                 />
+
                                 <ExternalLink
                                     title="odaibako"
                                     target="_blank"
                                     imgSrc={odaibako}
                                     linkSrc="https://odaibako.net/u/styfish"
+                                    gradationKey="slate"
                                 />
+
                                 <ExternalLink
                                     title="web store"
                                     target="_blank"
                                     imgSrc={webstore}
                                     linkSrc="https://chromewebstore.google.com/detail/drmaggot/lohndmdifcjjclnedjmljpmdjmfpbnai?authuser=1&hl=ja"
+                                    gradationKey="slate"
+                                />
+
+                                <ExternalLink
+                                    title="Github"
+                                    target="_blank"
+                                    imgSrc={github}
+                                    linkSrc="https://github.com/styfishnoob/dr.maggot"
+                                    gradationKey="black"
                                 />
                             </ul>
                         </li>
