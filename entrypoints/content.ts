e0cd4f52ac34d256a9034afd3d032cb116412751
styfishnoob <main.sakana@gmail.com>
2024-11-20 09:32:37 +0900

    followed-daysを追加

--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -1,3 +1,4 @@
+import { TwitchGql } from "@/src/common/followed-days/twitch/gql";
 export default defineContentScript({
     matches: [
         "*://*.youtube.com/*",
@@ -9,12 +10,13 @@ export default defineContentScript({
     allFrames: true,
     runAt: "document_idle",
 
-    main() {
+    async main() {
         import("@/src/common/auto-bonus/entrypoint");
         import("@/src/common/chat-filter/entrypoint");
         import("@/src/common/custom-css/entrypoint");
         import("@/src/common/danmaku/entrypoint");
         import("@/src/common/quick-block/entrypoint");
+        import("@/src/common/followed-days/entrypoint");
         import("@/src/common/sub-only/entrypoint");
     },
 });
