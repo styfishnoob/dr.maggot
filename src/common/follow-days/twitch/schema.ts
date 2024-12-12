ba849a95e5f1aaa8803d79d6741314d68c717fb3
styfishnoob <main.sakana@gmail.com>
2024-12-12 10:45:00 +0900

    Brand Typeを導入

--- src/common/follow-days/twitch/schema.ts
+++ src/common/follow-days/twitch/schema.ts
@@ -1,27 +1,39 @@
+export type UserID = Brand<string, "UserID">;
+export type UserLogin = Brand<string, "UserLogin">;
+export type ChannelID = UserID; // わかりやすいようチャンネルの方も型を作っておく
+export type ChannelLogin = UserLogin; // わかりやすいようチャンネルの型も作っておく
+
+export type FollowDaysMapItem = Map<ChannelLogin, number>; // number -> 経過日数
+export type FollowDaysMap = Map<UserLogin, FollowDaysMapItem>;
+export type ChannelIDMap = Map<ChannelLogin, UserID>; // ChannelIDはisModerator取得に必要
+export type ModeratorMap = Map<UserLogin, ChannelLogin>;
+
 export type QueryResponse = {
     data: any;
     extension: any;
 };
 
 export type User = {
-    id: string;
-    login: string;
+    id: UserID;
+    login: UserLogin;
     displayName: string;
+    follows: UserFollows;
+    isModerator: boolean;
+};
+
+export type UserFollows = {
+    edges: [FollowEdge];
+    pageInfo: PageInfo;
 };
 
 export type FollowEdge = {
     cursor: string;
     followedAt: string;
     node: {
-        login: string;
+        login: ChannelLogin;
     };
 };
 
 export type PageInfo = {
     hasNextPage: boolean;
-};
-
-export type UserFollows = {
-    edges: [FollowEdge];
-    pageInfo: PageInfo;
 };
