28b5fce9d04b632ef8e58fcc4a3f7656c1dcec5f
styfishnoob <main.sakana@gmail.com>
2024-12-12 10:45:57 +0900

    ファイル名変更に対応

--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -15,7 +15,7 @@ export default defineContentScript({
         import("@/src/common/custom-css/entrypoint");
         import("@/src/common/danmaku/entrypoint");
         import("@/src/common/quick-block/entrypoint");
-        import("@/src/common/followed-days/entrypoint");
+        import("@/src/common/follow-days/entrypoint");
         import("@/src/common/sub-only/entrypoint");
     },
 });
